01d31b75b8d9b12414cbfcd7a6ea1581bfaa714c
Harry Moore <harrymoore@yahoo.com>
2019-02-20 09:39:16 -0500

    cleanup

--- modules/ui/scripts/instance-icon.js
+++ modules/ui/scripts/instance-icon.js
@@ -5,9 +5,7 @@ define(function (require, exports, module) {
     // only register this handler on the Content instances page
     if (url && -1 !== url.indexOf('/content/cna:showcase')) {
         document.addEventListener("load", function (evt) {
-            // an image was loaded.
-            // if it looks like the preview image of a content instance then handle it
-            // http://localhost/preview/244853726?repository=127f0940f8f174c0b9ed&branch=285db7b759a4bfa4084a&node=4239867c07516c57dbe7&mimetype=image/jpeg&size=160&name=icon&attachment=default&fallback=http%3A%2F%2Flocalhost%2Foneteam%2Fmodules%2Fapp%2Fimages%2Fdoclib%2Fdocument-64.png
+            // an image was loaded. if it looks like the preview image of a content instance then handle it
             var srcUrl = "" + evt.target.src;
             var matches = srcUrl.match(/preview\/([^?]+)\?repository=([^&]+)\&branch=([^&]+)\&node=([^&]+)\&/);
 
@@ -17,7 +15,6 @@ define(function (require, exports, module) {
 
                 var nodeUrl = "/proxy/repositories/" + matches[2] + "/branches/" + matches[3] + "/nodes/query";
                 var query = '{"_doc": "_ID_","_fields": {"image.id": 1}}'.replace("_ID_", matches[4]);
-
                 
                 $.ajax({
                     type: "GET",
@@ -26,18 +23,14 @@ define(function (require, exports, module) {
                         "query": query
                     },
                     context: evt.target
-                    // dataType: "json",
-                    // contentType: "application/json; charset=utf-8",
                 }).done(function (data) {
                     var targetEl = this;
                     try {
                         // attempt to replace the icon preview url with a url that refers to the node's image property
-                        // console.log("Data Loaded: " + JSON.stringify(data, null, 4));
                         targetEl.src = "/proxy/repositories/_REPO_/branches/_BRANCH_/nodes/_NODE_/attachments/default".replace("_REPO_", matches[2]).replace("_BRANCH_", matches[3]).replace("_NODE_", data.rows[0].image.id);
                     } finally {}
                 });
             }
         }, true);
     }
-
 });
