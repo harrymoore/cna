793d7297e7aba1ad11f93cb1d1ca432245f2f69a
Harry Moore <harrymoore@yahoo.com>
2019-02-28 13:18:22 -0500

    updates

--- modules/ui/gadgets/cna-content-instances.js
+++ modules/ui/gadgets/cna-content-instances.js
@@ -1,6 +1,5 @@
 define(function(require, exports, module) {
     var Ratchet = require("ratchet/web");
-    var OneTeam = require("oneteam");
     var TemplateHelperFactory = require("template-helper");
     require("//code.jquery.com/ui/1.12.1/jquery-ui.js");
 
@@ -8,149 +7,140 @@ define(function(require, exports, module) {
 
     return Ratchet.GadgetRegistry.register("cna-content-instances", ContentInstancesGadget.extend({
 
-        getDefaultSortField: function(model)
-        {
-            return "sequence";
-        },
+        // getDefaultSortField: function(model)
+        // {
+        //     return "sequence";
+        // },
 
-        prepareModel: function(el, model, callback)
-        {
-            var self = this;
+        // prepareModel: function(el, model, callback)
+        // {
+        //     var self = this;
 
-            this.base(el, model, function() {
+        //     this.base(el, model, function() {
 
-                model.options.defaultSortDirection = 1;
+        //         model.options.defaultSortDirection = 1;
 
-                callback();
+        //         callback();
 
-            });
-        },
+        //     });
+        // },
 
         /**
          * Loads dynamic/configuration driven portions of the list into the model.
          *
          * @param model
          */
-        applyDynamicConfiguration: function(model)
-        {
-            var self = this;
+        // applyDynamicConfiguration: function(model)
+        // {
+        //     var self = this;
             
-            this.base(model);
+        //     this.base(model);
 
-            var sortSelectorGroupFields = model["selectorGroups"]["sort-selector-group"]["fields"];
-            sortSelectorGroupFields.unshift({
-                "key": "sequence",
-                "title": "Sequence",
-                "field": "sequence"
-            });
-        },
+        //     var sortSelectorGroupFields = model["selectorGroups"]["sort-selector-group"]["fields"];
+        //     sortSelectorGroupFields.unshift({
+        //         "key": "sequence",
+        //         "title": "Sequence",
+        //         "field": "sequence"
+        //     });
+        // },
 
 
-        beforeSwap: function(el, model, callback)
-        {
-            var self = this;
+        // beforeSwap: function(el, model, callback)
+        // {
+        //     var self = this;
 
-            this.base(el, model, function() {
-                callback();
-            });
-        },
+        //     this.base(el, model, function() {
+        //         callback();
+        //     });
+        // },
 
-        afterSwap: function(el, model, originalContext, callback)
-        {
-            var self = this;
-            this.thisModel = model;
+        // afterSwap: function(el, model, originalContext, callback)
+        // {
+        //     var self = this;
+        //     this.thisModel = model;
             
-            this.base(el, model, originalContext, function() {
+        //     this.base(el, model, originalContext, function() {
 
-                TemplateHelperFactory.afterRender(self, el);
-
-                // hide create button if nothing selected
-                var descriptor = self.observable("selected-content-type").get();
-                if (!descriptor)
-                {
-                    $(".btn.list-button-create-content").hide();
-                }
-
-                // drag and drop (jquery ui sortable)
-                $(el).find('.dataTable > tbody').sortable({
-                    update: function (event, ui) {
-                        console.log("Items re-sorted");
+        //         // drag and drop (jquery ui sortable)
+        //         $(el).find('.dataTable > tbody').sortable({
+        //             update: function (event, ui) {
+        //                 console.log("Items re-sorted");
     
-                        $(event.target.children).each(function (index, item) {
-                            console.log(index + " " + (item.id || "") + " " + (item.sequence || ""));
-                        });
+        //                 $(event.target.children).each(function (index, item) {
+        //                     console.log(index + " " + (item.id || "") + " " + (item.sequence || ""));
+        //                 });
     
-                        var nodeIds = [];
-                        $(event.target.children).map(function () {
-                            nodeIds.push(this.id);
-                        });
+        //                 var nodeIds = [];
+        //                 $(event.target.children).map(function () {
+        //                     nodeIds.push(this.id);
+        //                 });
     
-                        // find the content-instances gadget
-                        var gadget = Ratchet.Instances[$('div .gadget.content-instances').attr("ratchet")];
-                        var branch = gadget.observable("branch").get();
-                        var sortDirection = gadget.observable("sortDirection").get();
+        //                 // find the content-instances gadget
+        //                 var gadget = Ratchet.Instances[$('div .gadget.content-instances').attr("ratchet")];
+        //                 var branch = gadget.observable("branch").get();
+        //                 var sortDirection = gadget.observable("sortDirection").get();
     
-                        Chain(branch).queryNodes({
-                            _doc: {
-                                "$in": nodeIds
-                            },
-                            _fields: {
-                                sequence: 1,
-                                title: 1,
-                                isActive: 1
-                            }
-                        // }, {
-                        //     sort: {
-                        //         sequence: 1
-                        //     }
-                        }).then(function () {
-                            var result = this;
-                            var nodes = result.asArray();
-                            console.log("nodes: " + JSON.stringify(nodes, null, 4));
+        //                 Chain(branch).queryNodes({
+        //                     _doc: {
+        //                         "$in": nodeIds
+        //                     },
+        //                     _fields: {
+        //                         sequence: 1,
+        //                         title: 1,
+        //                         isActive: 1
+        //                     }
+        //                 // }, {
+        //                 //     sort: {
+        //                 //         sequence: 1
+        //                 //     }
+        //                 }).then(function () {
+        //                     var result = this;
+        //                     var nodes = result.asArray();
+        //                     console.log("nodes: " + JSON.stringify(nodes, null, 4));
     
-                            var patches = [];
-                            for (var i = 0; i < nodeIds.length; i++) {
-                                var sequence = 1+i;
-                                var node = result[nodeIds[i]];
-                                // if (node.isActive && node.isActive === "no") {
-                                //     // skip inactive nodes
-                                //     continue;
-                                // }
+        //                     var patches = [];
+        //                     for (var i = 0; i < nodeIds.length; i++) {
+        //                         var sequence = 1+i;
+        //                         var node = result[nodeIds[i]];
+        //                         // if (node.isActive && node.isActive === "no") {
+        //                         //     // skip inactive nodes
+        //                         //     continue;
+        //                         // }
     
-                                if (!Gitana.isEmpty(node.sequence) && node.sequence == sequence) {
-                                    // skip nodes whos order has not changed
-                                    continue;
-                                }
+        //                         if (!Gitana.isEmpty(node.sequence) && node.sequence == sequence) {
+        //                             // skip nodes whos order has not changed
+        //                             continue;
+        //                         }
     
-                                if (node.sequence !== sequence) {
-                                    // this node needs a new sequence. patch it.
-                                    patches.push({
-                                        node: node,
-                                        patch: {
-                                            op: Gitana.isEmpty(node.sequence) ? "add" : "replace",
-                                            path: "/sequence",
-                                            value: sequence
-                                        }
-                                    });
-                                }
-                            }
+        //                         if (node.sequence !== sequence) {
+        //                             // this node needs a new sequence. patch it.
+        //                             patches.push({
+        //                                 node: node,
+        //                                 patch: {
+        //                                     op: Gitana.isEmpty(node.sequence) ? "add" : "replace",
+        //                                     path: "/sequence",
+        //                                     value: sequence
+        //                                 }
+        //                             });
+        //                         }
+        //                     }
     
-                            // run any patches
-                            console.log("patches: " + JSON.stringify(patches, null, 4));
-                            async.each(patches, function(patch, callback) {
-                                Chain(patch.node).patch([patch.patch]).then(callback);        
-                            }, function(err) {
-                                // completed pathes
-                                console.log("patches completed");
-                                gadget.trigger("global_refresh");
-                            });
-                        });
-                    }
-                });
+        //                     // run any patches
+        //                     console.log("patches: " + JSON.stringify(patches, null, 4));
+        //                     async.each(patches, function(patch, callback) {
+        //                         Chain(patch.node).patch([patch.patch]).then(callback);        
+        //                     }, function(err) {
+        //                         // completed pathes
+        //                         console.log("patches completed");
+        //                         gadget.trigger("global_refresh");
+        //                     });
+        //                 });
+        //             }
+        //         });
     
-                callback();
-            });
-        }
+        //         callback();
+        //     });
+        // }
 
         // doGitanaQuery: function(context, model, searchTerm, query, pagination, callback)
         // {
