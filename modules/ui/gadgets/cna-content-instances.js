e4c44fce5dda877417cd68f44b354ce9ad75aa94
Harry Moore <harrymoore@yahoo.com>
2019-02-28 10:42:42 -0500

    module checkpoint (backup) commit

--- modules/ui/gadgets/cna-content-instances.js
+++ modules/ui/gadgets/cna-content-instances.js
@@ -1,134 +1,16 @@
 define(function(require, exports, module) {
-    // require("css!./content-instances.css");
-
     var Ratchet = require("ratchet/web");
-    var DocList = require("ratchet/dynamic/doclist");
     var OneTeam = require("oneteam");
     var TemplateHelperFactory = require("template-helper");
     require("//code.jquery.com/ui/1.12.1/jquery-ui.js");
-
-    var thisModel;
 
     var ContentInstancesGadget = require("oneteam/modules/app/gadgets/project/content/content-instances.js");
 
     return Ratchet.GadgetRegistry.register("cna-content-instances", ContentInstancesGadget.extend({
 
-        configureDefault: function()
-        {
-            this.base();
-
-            this.config({
-                "observables": {
-                    "query": "content-instances-list_query",
-                    "sort": "content-instances-list_sort",
-                    "sortDirection": "content-instances-list_sortDirection",
-                    "searchTerm": "content-instances-list_searchTerm",
-                    "selectedItems": "content-instances-list_selectedItems"
-                }
-            });
-        },
-
-        setup: function()
-        {
-            this.get("/projects/{projectId}/content", this.index);
-            this.get("/projects/{projectId}/content/{qname}", this.index);
-            this.get("/projects/{projectId}/content/{qname}/documents", this.index);
-        },
-
-        entityTypes: function()
-        {
-            return {
-                "plural": "content instances",
-                "singular": "content instance"
-            }
-        },
-
         getDefaultSortField: function(model)
         {
             return "sequence";
-        },
-
-        // prepareModel: function(el, model, callback)
-        // {
-        //     var self = this;
-
-        //     this.base(el, model, function() {
-
-        //         model.options.defaultSortDirection = 1;
-
-        //         callback();
-
-        //     });
-        // },
-
-        /**
-         * Loads dynamic/configuration driven portions of the list into the model.
-         *
-         * @param model
-         */
-        applyDynamicConfiguration: function(model)
-        {
-            var self = this;
-            
-            var selectedContentType = this.observable("selected-content-type").get();
-
-            // if (!model["selectorGroups"]) {
-            //     model["selectorGroups"] = {};
-            // }
-            // if (!model["selectorGroups"]["sort-selector-group"])
-            // {
-            //     model["selectorGroups"]["sort-selector-group"] = {};
-            // }
-            // if (!model["selectorGroups"]["sort-selector-group"]["fields"])
-            // {
-            //     model["selectorGroups"]["sort-selector-group"]["fields"] = [];
-            // }
-
-            // model["selectorGroups"]["sort-selector-group"]["fields"] = [];
-
-            var sortSelectorGroupFields = model["selectorGroups"]["sort-selector-group"]["fields"];
-            sortSelectorGroupFields.unshift({
-                "key": "sequence",
-                "title": "Sequence",
-                "field": "sequence"
-            });
-            // sortSelectorGroupFields.push({
-            //     "key": "title",
-            //     "title": "Title",
-            //     "field": "title"
-            // });
-
-            // "content-instances-buttons"
-            var buttons = OneTeam.configEvalArray(selectedContentType, "content-instances-buttons", self);
-            if (buttons && buttons.length > 0)
-            {
-                for (var i = 0; i < buttons.length; i++)
-                {
-                    model.buttons.push(buttons[i]);
-                }
-            }
-
-            // "content-instances-list-selected-actions"
-            var selectedActions = OneTeam.configEvalArray(self, "content-instances-list-selected-actions", self);
-            if (selectedActions && selectedActions.length > 0)
-            {
-                if (!model["selectorGroups"]) {
-                    model["selectorGroups"] = {};
-                }
-                if (!model["selectorGroups"]["multi-documents-action-selector-group"])
-                {
-                    model["selectorGroups"]["multi-documents-action-selector-group"] = {};
-                }
-                if (!model["selectorGroups"]["multi-documents-action-selector-group"]["actions"])
-                {
-                    model["selectorGroups"]["multi-documents-action-selector-group"]["actions"] = [];
-                }
-
-                for (var i = 0; i < selectedActions.length; i++)
-                {
-                    model["selectorGroups"]["multi-documents-action-selector-group"]["actions"].push(selectedActions[i]);
-                }
-            }
         },
 
         prepareModel: function(el, model, callback)
@@ -139,54 +21,38 @@ define(function(require, exports, module) {
 
                 model.options.defaultSortDirection = 1;
 
-                TemplateHelperFactory.create(self, "content-instances", function(err, renderTemplate) {
+                callback();
 
-                    model.renderTemplate = renderTemplate;
-
-                    callback();
-                });
             });
         },
+
+        /**
+         * Loads dynamic/configuration driven portions of the list into the model.
+         *
+         * @param model
+         */
+        applyDynamicConfiguration: function(model)
+        {
+            var self = this;
+            
+            this.base(model);
+
+            var sortSelectorGroupFields = model["selectorGroups"]["sort-selector-group"]["fields"];
+            sortSelectorGroupFields.unshift({
+                "key": "sequence",
+                "title": "Sequence",
+                "field": "sequence"
+            });
+        },
+
 
         beforeSwap: function(el, model, callback)
         {
             var self = this;
 
             this.base(el, model, function() {
-
-                // set up observables
-                var refreshHandler = self.refreshHandler(el);
-
-                // when the selected content type changes, we refresh
-                self.subscribe("selected-content-type", refreshHandler);
-
                 callback();
-
             });
-        },
-
-        checkPermission: function(observableHolder, permissionedId, permissionId, item)
-        {
-            var result = this.base(observableHolder, permissionedId, permissionId, item);
-
-            // should we do a capabilities check?
-            if (item.category === "capabilities-check")
-            {
-                result = false;
-
-                var descriptor = observableHolder.observable("selected-content-type").get();
-                if (descriptor && descriptor.capabilities)
-                {
-                    if (permissionId === "create_subobjects")
-                    {
-                        permissionId = "create";
-                    }
-
-                    result = descriptor.capabilities.contains(permissionId);
-                }
-            }
-
-            return result;
         },
 
         afterSwap: function(el, model, originalContext, callback)
@@ -284,100 +150,51 @@ define(function(require, exports, module) {
     
                 callback();
             });
-        },
-
-        doGitanaQuery: function(context, model, searchTerm, query, pagination, callback)
-        {
-            var self = this;
-
-            pagination.paths = true;
-
-            if (pagination.limit == 10) {
-                pagination.limit = 25;
-            }                                                                                                 
-
-            var project = self.observable("project").get();
-
-            if (OneTeam.isEmptyOrNonExistent(query) && searchTerm)
-            {
-                query = OneTeam.searchQuery(searchTerm, ["title", "description"]);
-            }
-
-            if (!query)
-            {
-                query = {};
-            }
-
-            OneTeam.projectBranch(self, function() {
-
-                // selected content type
-                var selectedContentTypeDescriptor = self.observable("selected-content-type").get();
-                if (!selectedContentTypeDescriptor)
-                {
-                    // produce an empty node map
-                    return callback(new Gitana.NodeMap(this));
-                }
-
-                query._type = selectedContentTypeDescriptor.definition.getQName();
-
-                // if (self.selectedLocale) {
-                //     query["_features.f:translation.locale"] = self.selectedLocale;
-                // }
-
-                this.queryNodes(query, pagination).then(function() {
-                    callback(this);
-                });
-            });
-        },
-
-        linkUri: function(row, model, context)
-        {
-            return OneTeam.linkUri(this, row);;
-        },
-
-        iconUri: function(row, model, context)
-        {
-            return OneTeam.iconUriForNode(row);
-        },
-
-        columnValue: function(row, item, model, context)
-        {
-            var self = this;
-
-            var project = self.observable("project").get();
-
-            var value = this.base(row, item, model, context);
-
-            row.pagination = model.pagination;
-
-            if (item.key === "titleDescription") {
-
-                value = model.renderTemplate(row);
-            }
-
-            return value;
-        },
-
-        populateSingleDocumentActions: function(row, item, model, context, selectorGroup)
-        {
-            var self = this;
-
-            var thing = Chain(row);
-
-            // evaluate the config space against the current row so that per-row action buttons customize per document
-            var itemActions = OneTeam.configEvalArray(thing, "content-instances-list-item-actions", self, null, null, true);
-
-            if (itemActions && itemActions.length > 0)
-            {
-                for (var z = 0; z < itemActions.length; z++)
-                {
-                    selectorGroup.actions.push(itemActions[z]);
-                }
-            }
-
-            // TODO: can't do this yet, need ACLs for every document?
-            //selectorGroup.actions = self.filterAccessRights(self, thing, model.buttons);
         }
+
+        // doGitanaQuery: function(context, model, searchTerm, query, pagination, callback)
+        // {
+        //     var self = this;
+
+        //     pagination.paths = true;
+
+        //     if (pagination.limit == 10) {
+        //         pagination.limit = 25;
+        //     }                                                                                                 
+
+        //     var project = self.observable("project").get();
+
+        //     if (OneTeam.isEmptyOrNonExistent(query) && searchTerm)
+        //     {
+        //         query = OneTeam.searchQuery(searchTerm, ["title", "description"]);
+        //     }
+
+        //     if (!query)
+        //     {
+        //         query = {};
+        //     }
+
+        //     OneTeam.projectBranch(self, function() {
+
+        //         // selected content type
+        //         var selectedContentTypeDescriptor = self.observable("selected-content-type").get();
+        //         if (!selectedContentTypeDescriptor)
+        //         {
+        //             // produce an empty node map
+        //             return callback(new Gitana.NodeMap(this));
+        //         }
+
+        //         query._type = selectedContentTypeDescriptor.definition.getQName();
+
+        //         // if (self.selectedLocale) {
+        //         //     query["_features.f:translation.locale"] = self.selectedLocale;
+        //         // }
+
+        //         this.queryNodes(query, pagination).then(function() {
+        //             callback(this);
+        //         });
+        //     });
+        // },
 
     }));
 
